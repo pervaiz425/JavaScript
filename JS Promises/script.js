280e6dcd33e8f701721ed5c845b537a24847220e
Muhammad Umar <umarpervaiz425@gmail.com>
2023-10-09 22:33:50 +0500

    Added comments to the code

--- JS Promises/script.js
+++ JS Promises/script.js
@@ -63,8 +63,18 @@ function watchTutorialPromise() {
 //Calling the function
 //Here the resolve and reject functions are the callback functions. 
 //and they return the value to the then and catch functions
-watchTutorialPromise().then(message => {
-    console.log('Success: ' + message);
-}).catch(error => {
-    console.log(error.name + ' ' + error.message);
-});
+//Let's explain how the promises works.
+//The promise function takes a callback function as an argument.
+//The callback function takes two arguments resolve and reject.
+//The resolve function is called when the promise is fulfilled.
+//The reject function is called when the promise is rejected.
+//The then function is called when the promise is fulfilled.
+//The catch function is called when the promise is rejected.
+//The then and catch functions takes a callback function as an argument.
+//If the resolve function is being called then the then function is called.
+//If the reject function is being called then the catch function is called.
+
+watchTutorialPromise().then(message => console.log('Success: ' + message))
+.catch(error => console.log(error.name + ' ' + error.message));
+
+
